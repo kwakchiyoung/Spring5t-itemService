b37cbe98b7299d0e214b1a9a541292b5b149c806
Luca Kwak2 <cldud03@naver.com>
2023-03-03 02:39:52 +0900

    수정-PC

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -18,23 +18,24 @@ public class BasicItemController {
     private final ItemRepository itemRepository;
 
     @GetMapping
-    public String items(Model model){
+    public String items(Model model){ //1.아이템조회
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items",items);
         return "basic/items";
     }
     @GetMapping("/{itemId}")
-    public String item(@PathVariable long itemId,Model model){
+    public String item(@PathVariable long itemId,Model model){ //2.아이템상세
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item",item);
         return "basic/item";
     }
     @GetMapping("/add")
-    public String addForm(){
+    public String addItemV1(){
         return "basic/addForm";
-    }
+    } //3 아이템등록 템플릿 호출
+
     //@PostMapping("/add")
-    public String save(@RequestParam String itemName,
+    public String save(@RequestParam String itemName,  //3.1 아이템등록
                        @RequestParam int price,
                        @RequestParam Integer quantity,
                        Model model){
@@ -43,11 +44,37 @@ public class BasicItemController {
         model.addAttribute("item",item);
         return "basic/item";
     }
-    @PostMapping("/add")
-    public String addItemV2(@ModelAttribute("item") Item item, Model model){
+    //@PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model){//3.2 아이템등록
         itemRepository.save(item);
-        //model.addAttribute("item",item); //@ModelAttribute("item")이부분이 알아서 이 기능까지 처리해준다. 자동 추가되어 생략 가능. 아니면 @ModelAttribute("item") Item item이렇게 사용하고 주석 제거
+        //model.addAttribute("item",item); //자동 추가되어 생략 가능.    //@ModelAttribute("item")이부분이 알아서 이 기능까지 처리해준다.
+        //  아니면 @ModelAttribute("item") Item item이렇게 사용하고 위에 주석을 제거,
+        //또는  @ModelAttribute("item") Item이렇게 @ModelAttribute 의 이름을 생략하면 모델에 저장될 때 클래스명을 사용한다. 이때 클래스의 첫글자만 소문자로 변경해서 자동등록된다.
+
         return "basic/item";
+    }
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item, Model model){ //3.3 아이템등록
+        itemRepository.save(item);
+        //또는  @ModelAttribute("item") Item이렇게 @ModelAttribute 의 이름을 생략하면 모델에 저장될 때 클래스명을 사용한다. 이때 클래스의 첫글자만 소문자로 변경해서 자동등록된다.
+        return "basic/item";
+    }
+    @PostMapping("/add") //3.4아이템등록
+    public String addItemV4(Item item, Model model){ //@ModelAttribute 자체도 생략가능하다. 대상 객체는 모델에 자동 등록된다.
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    @GetMapping("/{itemId}/edit") //4.수정
+    public String editForm(@PathVariable Long itemId , Model model){ //@PathVariable("itemId") <-변수명이 같으면 생략가능.
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item",item);
+        return "basic/editForm";
+    }
+    @PostMapping("/{itemId}/edit") //4.수정
+    public String edit(@PathVariable Long itemId , @ModelAttribute Item item){
+        itemRepository.update(itemId,item);
+        return "redirect:/basic/items/{itemId}";
     }
 
 
